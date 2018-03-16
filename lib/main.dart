d67fc1030e367b5fe603f246e0f4992dcc36492c
ysreedhar <ya.sree@gmail.com>
2018-03-16 11:37:44 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  MyAppState createState() => new MyAppState();
+
+}
+
+
+class MyAppState extends State<MyApp>{
+  @override
+
+  Widget build(BuildContext context){
+    return new  Scaffold(
+
+    );
+  }
+
+}
