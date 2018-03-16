29f43bff6e5d3e94a5038099cd748d6488adcb75
ysreedhar <ya.sree@gmail.com>
2018-03-16 12:00:30 +0800

    Finished Code Snippet

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,42 @@ class MyApp extends StatefulWidget{
 class MyAppState extends State<MyApp>{
   @override
 
+  bool _isChecked = false;
+
+  void onChanged(bool value){
+    setState((){
+      _isChecked=value;
+    });
+  }
+
   Widget build(BuildContext context){
     return new  Scaffold(
+      appBar: new AppBar(
+        title: new Text('CheckBoxes with Flutter'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Row(
+                children: <Widget>[
+                  new Text('Click the Checkbox'),
+                  new Checkbox(value: _isChecked, onChanged: (bool value){onChanged(value);}),
+                ],
+              ),
+              new CheckboxListTile(
+                title: new Text('Click this->'),
+                  activeColor: Colors.red,
+                  secondary: const Icon(Icons.home),
+                  value: _isChecked,
+                  onChanged: (bool value){onChanged(value);})
+
+            ],
+          ),
+        ),
+      ),
+
 
     );
   }
